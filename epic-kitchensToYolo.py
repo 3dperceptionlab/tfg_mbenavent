9b350c4039f15b12ecfddf77eef4ca0878d63358
Manuel Benavent <mbenavent99@gmail.com>
2021-03-20 21:13:03 +0100

    Ajustes script

--- epic-kitchensToYolo.py
+++ epic-kitchensToYolo.py
@@ -1,5 +1,4 @@
 import csv
-import csv
 import sys
 from collections import namedtuple
 import ast
@@ -10,6 +9,7 @@ if (len(sys.argv) != 3):
     exit()
 
 classes = []
+print('-------- PROCESSING CLASSES --------')
 with open(sys.argv[1]) as csv_file:
     with open('epic-kitchens_classes.txt','w') as classes_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
@@ -25,12 +25,12 @@ with open(sys.argv[1]) as csv_file:
                 classes_file.write(str(row[1]) + '\n')
                 classes.append(row[1])
                 line_count += 1
-        print(f'Processed {line_count} lines.')
+        print(f'Processed {line_count} classes.')
 
 
+print('-------- PROCESSING LABELS --------')
 labels_dict = {}
 LabelData = namedtuple("LabelData", "object_class bounding_box")
-
 with open(sys.argv[2]) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
@@ -41,7 +41,7 @@ with open(sys.argv[2]) as csv_file:
         else:
             location = '/epic-kitchens/' + row[2] + '/object_detection_images/' + row[4] + '.jpg'
             bounding_boxes = x = ast.literal_eval(row[5])
-            object_class = row[0] # we use id in order to avoid errors
+            object_class = row[0] # Use id, a same class may have different names
             partial_labels = []
             for bb in bounding_boxes:
                 label = LabelData(object_class=object_class, bounding_box=bb)
@@ -57,11 +57,17 @@ with open(sys.argv[2]) as csv_file:
                 new_boundingboxes = old_boundingboxes + partial_labels
                 labels_dict[location] = new_boundingboxes
             line_count += 1
-    print(f'Processed {line_count} lines.')
+    print(f'Processed {line_count} epic-kicthens labels.')
 
+
+print('-------- SAVING LABELS IN YOLO FORMAT --------')
 with open('train.txt','w') as train_file:
     for key in labels_dict:
         train_file.write(key + ' ')
         for label in labels_dict[key]:
             train_file.write(str(label.bounding_box[1]) + ',' + str(label.bounding_box[0]) + ',' + str(label.bounding_box[1] + label.bounding_box[3]) + ',' +  str(label.bounding_box[0] + label.bounding_box[2]) + ',' + str(label.object_class) + ' ')
         train_file.write('\n')
+
+print('-------- COMPLETED --------')
+print('epic-kitchens_classes.txt contains the classes that should be placed in the model folder.')
+print('train.txt contains the labels for training.')
