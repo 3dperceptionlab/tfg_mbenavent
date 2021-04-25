478d975cd876e024fa8529e2432a07240f6311ce
Manuel <mbenavent99@gmail.com>
2021-04-25 13:33:58 +0200

    epickitchens test

--- EPIC-KITCHENS/epickitchens_to_YOLO.py
+++ EPIC-KITCHENS/epickitchens_to_YOLO.py
@@ -20,6 +20,7 @@ for index, row in interactive_nouns.iterrows():
 # Filter labels to be used
 labels = pd.read_csv('downloaded-labels/EPIC_train_object_labels.csv')
 labels = labels.loc[labels['noun_class'].isin(interactive_objects)]
+test_labels = labels.loc[labels['participant_id'].isin(['P05','P06','P07'])]
 labels = labels.loc[labels['participant_id'].isin(['P01','P02','P03','P04'])]
 
 # Get the 10 classes with the most labels
@@ -35,41 +36,56 @@ for key, value in dictionary.items():
     final_objects.add(key)
 
 labels = labels.loc[labels['noun_class'].isin(final_objects)]
+test_labels = test_labels.loc[test_labels['noun_class'].isin(final_objects)]
 
 # Process labels and classes
-labels_dict = {}
 class_dict = {}
-LabelData = namedtuple('LabelData', 'object_class bounding_box')
 ClassData = namedtuple('ClassData', 'new_id name')
-for index, label in labels.iterrows():
-    location = '/datasets/EPIC-KITCHENS/' + label['participant_id'] + '/object_detection_images/' + label['video_id'] + '/' + str(label['frame']).rjust(10, '0') + '.jpg'
-    bounding_boxes = ast.literal_eval(label['bounding_boxes'])
-    object_class = label['noun_class'] # Use id, a same class may have different names
+def process_dataset(labels):
+    labels_dict = {}
+    LabelData = namedtuple('LabelData', 'object_class bounding_box')
+    for index, label in labels.iterrows():
+        location = '/datasets/EPIC-KITCHENS/' + label['participant_id'] + '/object_detection_images/' + label['video_id'] + '/' + str(label['frame']).rjust(10, '0') + '.jpg'
+        bounding_boxes = ast.literal_eval(label['bounding_boxes'])
+        object_class = label['noun_class'] # Use id, a same class may have different names
 
-    # Save classes in dictionary
-    if object_class not in class_dict:
-        class_data = ClassData(new_id=len(class_dict), name=nouns_list[object_class]) # Save the first noun name
-        class_dict[object_class] = class_data
-        object_class = class_data.new_id # Save new_id to print into file
-    else:
-        object_class = class_dict[object_class].new_id
+        # Save classes in dictionary
+        if object_class not in class_dict:
+            class_data = ClassData(new_id=len(class_dict), name=nouns_list[object_class]) # Save the first noun name
+            class_dict[object_class] = class_data
+            object_class = class_data.new_id # Save new_id to print into file
+        else:
+            object_class = class_dict[object_class].new_id
 
-    # Save labels in dictionary
+        # Save labels in dictionary
 
-    partial_labels = []
-    for bb in bounding_boxes:
-        label = LabelData(object_class=object_class, bounding_box=bb)
-        partial_labels.append(label)
-    if len(bounding_boxes) == 0:
-        continue
-    if location not in labels_dict:
-        # Add
-        labels_dict[location] = partial_labels
-    else:
-        # Modify
-        old_boundingboxes = labels_dict[location]
-        new_boundingboxes = old_boundingboxes + partial_labels
-        labels_dict[location] = new_boundingboxes
+        partial_labels = []
+        for bb in bounding_boxes:
+            label = LabelData(object_class=object_class, bounding_box=bb)
+            partial_labels.append(label)
+        if len(bounding_boxes) == 0:
+            continue
+        if location not in labels_dict:
+            # Add
+            labels_dict[location] = partial_labels
+        else:
+            # Modify
+            old_boundingboxes = labels_dict[location]
+            new_boundingboxes = old_boundingboxes + partial_labels
+            labels_dict[location] = new_boundingboxes
+    return labels_dict
+
+
+
+
+print('-------- SAVING LABELS IN YOLO FORMAT --------')
+with open('processed-labels/train.txt','w') as train_file:
+    labels_dict = process_dataset(labels)
+    for key in labels_dict:
+        train_file.write(key + ' ')
+        for label in labels_dict[key]:
+            train_file.write(str(label.bounding_box[1]) + ',' + str(label.bounding_box[0]) + ',' + str(label.bounding_box[1] + label.bounding_box[3]) + ',' +  str(label.bounding_box[0] + label.bounding_box[2]) + ',' + str(label.object_class) + ' ')
+        train_file.write('\n')
 
 print('-------- SAVING CLASSES IN YOLO FORMAT --------')
 sorted_class_dict = {k: v for k, v in sorted(class_dict.items(), key=lambda item: item[1].new_id)} # Inserted sorted but just in case
@@ -77,9 +93,8 @@ with open('processed-labels/epic-kitchens_classes.txt','w') as class_file:
     for key, value in sorted_class_dict.items():
         class_file.write(value.name + '\n')
 
-
-print('-------- SAVING LABELS IN YOLO FORMAT --------')
-with open('processed-labels/train.txt','w') as train_file:
+with open('processed-labels/test.txt','w') as train_file:
+    labels_dict = process_dataset(test_labels)
     for key in labels_dict:
         train_file.write(key + ' ')
         for label in labels_dict[key]:
