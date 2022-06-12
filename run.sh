e42935e27166e2a2729eb4543a613180dcab16fb
Manuel <mbenavent99@gmail.com>
2022-06-12 13:10:30 +0200

    Docker run script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export containerName=yolokeras_$USER
+sleep 3 && \
+        xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerName` >/dev/null 2>&1 &
+docker run -d --gpus '"device=1"' --rm -it \
+	--volume="/home/mbenavent/workspace:/workspace:rw" \
+	--volume="/mnt/md1/datasets/:/datasets:ro" \
+	--volume=$HOME/.Xauthority:/root/.Xauthority:ro \
+	--workdir="/workspace" \
+	-v $XSOCK:$XSOCK:rw \
+	--net=host \
+        --env="DISPLAY" \
+	--name $containerName \
+	mbenavent/david8862-yolokeras:latest bash
